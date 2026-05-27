6140aeda7e32766396d1b1efa8bc7319f3865f81
wheeler-bryan <bswheeler@wpi.edu>
2026-05-27 12:58:21 -0400

    Trophy picture updated

--- world-cup-predictor/src/Components/ChampionDisplay.tsx
+++ world-cup-predictor/src/Components/ChampionDisplay.tsx
@@ -7,13 +7,13 @@ export function ChampionDisplay( { champion } : {
     return (
         <div>
             {(champion) ?
-                <div className="flex flex-col justify-center items-center rounded-2xl bg-gray-300 m-[3rem] pb-[2rem]">
+                <div className="flex flex-col justify-center items-center rounded-2xl bg-gray-300 m-[1rem] md:m-[3rem] pb-[2rem]">
                     <h1 className="text-center mt-[2rem]">YOUR 2026 FIFA WORLD CUP CHAMPION</h1>
                     <h1 className="text-center">{champion.name}</h1>
-                    <div className="flex justify-center gap-2 pt-[1rem]">
-                        <img src={"trophy.png"} alt={"trophy1"} height="200rem" width="200rem" />
+                    <div className="flex justify-center pt-[1rem]">
+                        <img src={"trophy.png"} alt={"trophy1"} height="150rem" width="140rem" />
                         <img src={champion.flag} alt={champion.abbreviation} height="200rem" width="150rem" />
-                        <img src={"trophy.png"} alt={"trophy2"} height="200rem" width="200rem" />
+                        <img src={"trophy.png"} alt={"trophy2"} height="150rem" width="140rem" />
                     </div>
                 </div> : null}
         </div>
