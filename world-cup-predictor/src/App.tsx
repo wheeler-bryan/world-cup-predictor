f6c65a385ee99b1b0c677241e1236d6f32396952
wheeler-bryan <bswheeler@wpi.edu>
2026-06-11 18:53:07 -0400

    Blocking other Routes

--- world-cup-predictor/src/App.tsx
+++ world-cup-predictor/src/App.tsx
@@ -1,8 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import { Home } from './Routes/Home.tsx'
-import { MakePicks } from "./Routes/MakePicks.tsx";
-import { BracketSubmitted } from "./Routes/BracketSubmitted";
 import { Leaderboard } from "./Routes/Leaderboard.tsx";
 import { ProductionLeaderboard } from "./Routes/ProductionLeaderboard.tsx";
 
@@ -12,10 +9,7 @@ function App() {
     <>
         <BrowserRouter>
           <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/makepicks" element={<MakePicks />} />
-              <Route path="/bracket_submitted" element={<BracketSubmitted />} />
-              <Route path="/leaderboard" element={<Leaderboard />} />
+              <Route path="/" element={<Leaderboard />} />
               <Route path="/leaderboard_testing" element={<ProductionLeaderboard />} />
           </Routes>
         </BrowserRouter>
