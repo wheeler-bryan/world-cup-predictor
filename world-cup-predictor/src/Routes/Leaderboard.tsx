e18337072204bec3e8ca65604d8a6cc3151020c3
wheeler-bryan <bswheeler@wpi.edu>
2026-06-11 18:55:15 -0400

    Blocked routes

--- world-cup-predictor/src/Routes/Leaderboard.tsx
+++ world-cup-predictor/src/Routes/Leaderboard.tsx
@@ -6,8 +6,8 @@ export function Leaderboard() {
         <div className="bg-[url('/world-cup-background.jpg')] bg-cover bg-center h-screen">
             <div className="flex flex-col items-center justify-center h-screen">
                 <div className="bg-white flex shadow-2xl shadow-black flex-col items-center justify-center rounded-4xl p-8">
-                    <h2 className="text-center">Thanks for submitting a bracket! Leaderboard coming June 11th!</h2>
-                    <h3 className="text-center">To edit your picks, or view before June 11th, use your code to access your account.</h3>
+                    <h2 className="text-center">Thanks for submitting a bracket!</h2>
+                    <h3 className="text-center">Beta leaderboard to view all picks coming June 11th!</h3>
                 </div>
             </div>
         </div>
