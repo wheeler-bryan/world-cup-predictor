5667eb7a57921102f87c1574af3e5c234c3a1ce6
wheeler-bryan <bswheeler@wpi.edu>
2026-07-10 17:01:17 -0400

    France Spain QF

--- world-cup-predictor/src/assets/results.ts
+++ world-cup-predictor/src/assets/results.ts
@@ -257,10 +257,10 @@ function score_round_of_16(qf: MatchupData[]): [number, number] {
 }
 
 export const winners_qf: Country[] = [
-    fakeCountry, fakeCountry, fakeCountry, fakeCountry
+    groupI[0], groupH[0], fakeCountry, fakeCountry
 ]
 export const losers_qf: Country[] = [
-    fakeCountry, fakeCountry, fakeCountry, fakeCountry
+    groupC[1], groupG[0], fakeCountry, fakeCountry
 ]
 export const eliminated_ro16: Country[] = [eliminated_ro32, losers_round_of_16].flat();
 
